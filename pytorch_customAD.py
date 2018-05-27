52b15e31d47d3bda98f16f6aee085788203e6d6b
stesha2016 <xuchen@t2mobile.com>
2018-05-27 20:37:59 +0800

    init

---
+++ pytorch_customAD.py
@@ -0,0 +1,40 @@
+import torch
+
+class MyReLU(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx,input):
+        ctx.save_for_backward(input)
+        return input.clamp(min=0)
+
+    @staticmethod
+    def backward(ctx,grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input<0] = 0
+        return grad_input
+
+dtype = torch.float
+device = torch.device("cpu")
+
+N,D_in,H,D_out = 64,1000,100,10
+x = torch.randn(N,D_in,device=device,dtype=dtype)
+y = torch.randn(N,D_out,device=device,dtype=dtype)
+
+w1 = torch.randn(D_in,H,device=device,dtype=dtype,requires_grad=True)
+w2 = torch.randn(H,D_out,device=device,dtype=dtype,requires_grad=True)
+
+learning_rete = 1e-6
+for t in range(50000):
+    relu = MyReLU.apply
+
+    y_pred = relu(x.mm(w1).mm(w2))
+    loss = (y_pred - y).pow(2).sum()
+    print(t,loss.item())
+    loss.backward()
+
+    with torch.no_grad():
+        w1 -= learning_rete * w1.grad
+        w2 -= learning_rete * w2.grad
+
+        w1.grad.zero_()
+        w2.grad.zero_()
